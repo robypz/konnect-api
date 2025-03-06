5f8532722546c64977d8cc5b12889e7c8a5919b3
robypz <robertyepez0208@hotmail.com>
2025-03-06 11:33:00 +0100

    Add models, factories, seeders, and request classes for Seller, Catalog, Category, and Products

---
+++ database/migrations/2025_03_06_102315_create_categories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use MongoDB\Laravel\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $collection) {
+            $collection->id();
+            $collection->string('name');
+            $collection->string('description');
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
