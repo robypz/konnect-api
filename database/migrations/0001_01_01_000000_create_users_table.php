8385c61e6ba65dc88fd7788993c391fd2a6f4766
robypz <robertyepez0208@hotmail.com>
2025-03-18 14:43:53 +0100

    Refactor user-related migration files to use consistent naming for Blueprint instances

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,29 +11,27 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('users', function (Blueprint $collection) {
+            $collection->string('name');
+            $collection->string('email')->unique();
+            $collection->timestamp('email_verified_at')->nullable();
+            $collection->string('password');
+            $collection->rememberToken();
+            $collection->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('password_reset_tokens', function (Blueprint $collection) {
+            $collection->string('email')->primary();
+            $collection->string('token');
+            $collection->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+        Schema::create('sessions', function (Blueprint $collection) {
+            $collection->foreignId('user_id')->nullable()->index();
+            $collection->string('ip_address', 45)->nullable();
+            $collection->text('user_agent')->nullable();
+            $collection->longText('payload');
+            $collection->integer('last_activity')->index();
         });
     }
 
