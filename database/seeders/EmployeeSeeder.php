aeab664074c0970a3bc1564c0bcba38a92e39f3a
Robert Yepez <robertyepez0208@hotmail.com>
2025-08-04 08:55:38 +0200

    "init"

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -13,6 +13,6 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-
+        //
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use RobYpz\MongoRole\Models\Role;
 
@@ -14,16 +13,9 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Employee',
-            'email' => 'employee@konnect.com',
-        ])->roles()->attach(Role::where('name', 'employee')->first());
-
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@konnect.com',
-        ])->roles()->attach(Role::where('name', 'admin')->first());
+            'name' => 'root',
+            'email' => 'root@konnect.com',
+        ])->roles()->attach(Role::where('name', 'root')->first());
     }
 }
