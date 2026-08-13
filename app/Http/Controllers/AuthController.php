4dc7d73623bbb55b10b152e0b470cdee297cad3d
robypz <robertyepez0208@hotmail.com>
2026-08-13 12:32:30 +0200

    feat: add AuthController and implement Laravel MCP integration with Docker support

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\auth\SigninRequest;
+use App\Http\Requests\auth\SignupRequest;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+
+
+class AuthController extends Controller
+{
+    public function signin(SigninRequest $request)
+    {
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json($user->createToken($request->device_name)->plainTextToken, 200);
+    }
+
+    public function signup(SignupRequest $request) {}
+
+    public function signout() {}
+
+    public function sendPasswordResetLink(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return response()->json($status, 200);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ]);
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return response()->json($status, 200);
+    }
+
+    public function user(Request $request)
+    {
+        $user = User::with(['employee','roles'])->find($request->user()->id);
+        return response()->json($user, 200);
+    }
+}
